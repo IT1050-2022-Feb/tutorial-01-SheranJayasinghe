79ac8abbd6b58ffb7cda4495b51ea4dba891c951
H.D.S.C.Jayasinghe <127472063+SheranJayasinghe@users.noreply.github.com>
2023-03-10 06:54:04 +0000

    Completed tute 1,2,3,4

--- .ccls-cache/@home@runner@tutorial-01-SheranJayasinghe/Tute03.c
+++ .ccls-cache/@home@runner@tutorial-01-SheranJayasinghe/Tute03.c
@@ -13,18 +13,6 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
-
-  int n,i,sum=0;
-
-  printf("Enter Number : ");
-  scanf("%d",&n);
-
-  for(i = 0; i <= n; i++)
-    {
-      sum = sum + i;
-    }
-
-  printf("Sum : %d",sum);
   
   return 0;
 }
